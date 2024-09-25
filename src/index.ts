34b27a6e992cd6a1365fca8c720e7da736f004c3
wangsx-c <wangsx-c@glodon.com>
2024-09-25 22:34:40 +0800

    feat(parser): use xstate as parser method

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import pdfjs, { PDFDocumentProxy } from 'pdfjs-dist'
 import { PDFOperatorList } from 'pdfjs-dist/types/src/display/api';
-
+import runner from './state'
 let doc:PDFDocumentProxy;
 
 async function makeObjects(opsList:PDFOperatorList) {
-    
+    for (let index = 0;index< opsList.fnArray.length;index++) {
+        runner.send({type: '' + opsList.fnArray[index], value: opsList.argsArray[index]})
+    }
 }
 async function getPageInfo(pageNum:number) {
     const page = await doc.getPage(pageNum)
--- src/state.ts
+++ src/state.ts
@@ -20,3 +20,4 @@ const countMachine = createMachine({
         },
     },
   });
+ export default createActor(countMachine).start();
